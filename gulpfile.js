3a237aa9f7b27cc04d804754da58dff32e8519e9
Shubhankar Dandekar <shubhankard34@gmail.com>
2016-07-19 15:02:06 +0530

    Add script.js using gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,19 @@
 var gulp = require('gulp'); 
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
+var concat = require('gulp-concat');
+
+var coffeeSources = ['components/coffee/tagline.coffee'];
+
+var jsSources = ['components/scripts/pixgrid.js', 'components/scripts/rclick.js','components/scripts/tagline.js','components/scripts/template.js'];
 
 gulp.task('coffee', function(){
-   gulp.src('components/coffee/tagline.coffee').pipe(coffee({bare: true}).on('error', gutil.log)).pipe(gulp.dest('components/scripts')) 
+   gulp.src(coffeeSources).pipe(coffee({bare: true}).on('error', gutil.log)).pipe(gulp.dest('components/scripts')) 
+});
+
+gulp.task('js', function(){
+   gulp.src(jsSources)
+   .pipe(concat('scripts.js'))
+   .pipe(gulp.dest('builds/development/js'))
+    
 });
