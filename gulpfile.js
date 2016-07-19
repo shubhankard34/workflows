2a0faa7213582563378cdfe54153b665377f0f34
Shubhankar Dandekar <shubhankard34@gmail.com>
2016-07-19 14:49:29 +0530

    Run gulp coffee

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,7 @@
 var gulp = require('gulp'); 
 var gutil = require('gulp-util');
+var coffee = require('gulp-coffee');
 
-gulp.task('log', function(){
-    
-    gutil.log('Workflows are awesome.!');
-    
+gulp.task('coffee', function(){
+   gulp.src('components/coffee/tagline.coffee').pipe(coffee({bare: true}).on('error', gutil.log)).pipe(gulp.dest('components/scripts')) 
 });
